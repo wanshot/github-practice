47b7c3f3769a67f5d448017cbfbe10af44f45ca4
wan <nishikawa0228@sj9.so-net.ne.jp>
2018-11-29 13:08:19 +0900

    refs #6

--- sample.py
+++ sample.py
@@ -3,3 +3,7 @@ print('Hello hoge')
 print('Hello huga')
 sssssssssssssss33
 ssssssss
+
+
+https://bp.lvh.me:8000/s/00000
+https://bp.tracery.jp/s/assdd
---
+++ test.py
@@ -0,0 +1,2 @@
+https://lvh.me:8000/s/00000
+https://bp.tracery.jp/s/assdd
