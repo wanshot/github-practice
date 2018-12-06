b891893ac5568636a61e72ae0c0c38c7ef994109
wan <nishikawa0228@sj9.so-net.ne.jp>
2018-12-06 12:24:30 +0900

    refs #11

---
+++ test3.py
@@ -0,0 +1,3 @@
+http://6797a4dc.ngrok.io/s/a5b17a1a9c5541449dde14b6fe10b397
+https://6797a4dc.ngrok.io/s/a5b17a1a9c5541449dde14b6fe10b397
+http://tracery.jp/s/a5b17a1a9c5541449dde14b6fe10b397
---
+++ test5.py
@@ -0,0 +1,1 @@
++++ http://6797a4dc.ngrok.io/s/a5b17a1a9c5541449dde14b6fe10b397
