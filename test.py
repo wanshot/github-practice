0db744338b7b14e1646757756cc6ebfdd48405c9
wan <nishikawa0228@sj9.so-net.ne.jp>
2018-12-13 14:54:04 +0900

    refs #16

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ def hoge():
     https://tracery.jp/s/43b7bf1a1cb84a3cad4c7aed979f5933
     http://lvh.me:8000/s/5e86dac8613b48bfab23bf4fe2489c2c
     http://75c606fb.ngrok.io/s/5e86dac8613b48bfab23bf4fe2489c2c
+    aaa
     """
     pass
