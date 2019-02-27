132d3e28888b22a9fb5f9f6a1647d18e0488fdc3
wan <nishikawa0228@sj9.so-net.ne.jp>
2019-02-27 17:51:04 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,11 +1,3 @@
-print 'Hello Joe'
-print('Hello hoge')
-print('Hello huga')
-sssssssssssssss33
-ssssssss
-tes
-ssssss
-http://lvh.me:8000/s/5d62db44cbfa491caeca6b883bbb5ae8
-test
+
 http://faf53d3f.ngrok.io/s/5d62db44cbfa491caeca6b883bbb5ae8
 test
