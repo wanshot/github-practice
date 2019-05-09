6c84b8cea19aa6651ebb5cd5c2344089c256c25d
wan <nishikawa0228@sj9.so-net.ne.jp>
2019-05-09 11:54:09 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ print 'Hello Joe'
 print('Hello hoge')
 print('Hello huga')
 sssssssssssssss33
-sssssss
+ssssss
 http://lvh.me:8000/s/704d1665c5004ae0987e282c67984a9a
 http://06ecc1a7.ngrok.io/s/704d1665c5004ae0987e282c67984a9a
 http://lvh.me:8000/s/012d65f80b484f629ae24dce8c189cdd
