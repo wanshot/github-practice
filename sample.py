8be13f2b456e2dabfc6f4a8b40045c6868522154
wan <nishikawa0228@sj9.so-net.ne.jp>
2019-05-09 12:21:04 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,9 +4,8 @@ print('Hello huga')
 sssssssssssssss33
 s
 
-http://lvh.me:8000/s/704d1665c5004ae0987e282c67984a9a
-http://06ecc1a7.ngrok.io/s/704d1665c5004ae0987e282c67984a9a
 http://lvh.me:8000/s/012d65f80b484f629ae24dce8c189cdd
 http://06ecc1a7.ngrok.io/s/012d65f80b484f629ae24dce8c189cdd
 http://06ecc1a7.ngrok.io/s/8e9a8a3407a944f0977a57790d3c1556
 http://06ecc1a7.ngrok.io/s/cf31b526b7da4c259c3b95c83f06fd7d
+http://06ecc1a7.ngrok.io/s/d8df75359c6b41f6a31ddc8701360577
