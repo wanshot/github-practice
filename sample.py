e2380063d2a00fcccae09e0493a2c4f5e09e791e
wan <nishikawa0228@sj9.so-net.ne.jp>
2019-05-09 11:20:08 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,3 +7,4 @@ http://lvh.me:8000/s/704d1665c5004ae0987e282c67984a9a
 http://06ecc1a7.ngrok.io/s/704d1665c5004ae0987e282c67984a9a
 http://lvh.me:8000/s/012d65f80b484f629ae24dce8c189cdd
 http://06ecc1a7.ngrok.io/s/012d65f80b484f629ae24dce8c189cdd
+http://06ecc1a7.ngrok.io/s/8e9a8a3407a944f0977a57790d3c1556
