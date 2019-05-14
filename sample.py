b8a98625c74fd4b89fa43b66728edccb272039eb
wan <nishikawa0228@sj9.so-net.ne.jp>
2019-05-14 16:18:36 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -9,3 +9,4 @@ http://b11547c4.ngrok.io/s/012d65f80b484f629ae24dce8c189cdd
 http://b11547c4.ngrok.io/s/8e9a8a3407a944f0977a57790d3c1556
 http://b11547c4.ngrok.io/s/cf31b526b7da4c259c3b95c83f06fd7d
 http://b11547c4.ngrok.io/s/d8df75359c6b41f6a31ddc8701360577
+http://09129951.ngrok.io/s/6d42d5d922974ab38b9a8a548cecd25b
