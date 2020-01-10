68ad9f8ff4088bc7ec053d8f7af682bffe8bf949
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-01-10 09:30:41 +0900

    refs #master

--- sample.py
+++ sample.py
@@ -1,31 +1,12 @@
-print 'Hello Joe'
-print('Hello hoge')
-print('Hello huga')
-sssssssssssssss33
-ssssssss
-tes
-ssssssaa
-sss
-¥n
-\n
-ss
-http://lvh.me:8000/s/7519b7cddfbd4661bbc0cd03146fba6a
-s
-s
-
-http://bp.lvh.me:8000/projects/test/pages/2
-https://bp.tracery.jp/projects/test/pages/2
-  
-  sss
-http://lvh.me:8000/s/7519b7cddfbd4661bbc0cd03146fba6a
-https://tracery.jp/s/3b1967330d634cc58cb00226b776914a
-aa
-
-http://lvh.me:8000/s/7519b7cddfbd4661bbc0cd03146fba6a
-aaa
-aaaaaaaaaaaaaaa
-http://lvh.me:8000/s/7519b7cddfbd4661bbc0cd03146fba6a
-
-http://lvh.me:8000/s/7519b7cddfbd4661bbc0cd03146fba6asasasajjj
 
 
+def permalink():
+    """http://lvh.me:8000/s/ef83e38a72994a75bb4d097bfa5418dc
+    """
+    return
+
+
+def url():
+    """http://bp.lvh.me:8000/projects/test/pages/2
+    """
+    return
--- test1.py
+++
@@ -1,1 +0,0 @@
- http://lvh.me:8000/s/7519b7cddfbd4661bbc0cd03146fba6asasasajjj
