0d1c07a7deb4f68660d6fac6306b5c52ab178ab9
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-01-10 11:06:19 +0900

    master change

--- sample.py
+++ sample.py
@@ -1,3 +1,10 @@
+def permalink():
+    """http://lvh.me:8000/s/ef83e38a72994a75bb4d097bfa5418dc
+    """
+    return
 
 
-
+def url():
+    """http://bp.lvh.me:8000/projects/test/pages/2
+    """
+    return
