ef2ff590f84985540c464ee75718ab4448acd232
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-01-14 09:40:12 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,2 +1,1 @@
-https://beproud.tracery.jp/projects/tracery/pages/1
-https://beproud.tracery.jp/projects/tracery/pages/1
+https://beproud.tracery.jp/projects/tracery/pages/1 https://beproud.tracery.jp/projects/tracery/pages/1
