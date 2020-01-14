e52d2b0f8ee45e3ceee6a3a3fc1e32a9767858fe
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-01-14 09:32:45 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,2 +1,1 @@
-https://tracery.jp/s/ef83e38a72994a75bb4d097bfa5418dc
-https://tracery.jp/s/ef83e38a72994a75bb4d097bfa5418dc
+https://tracery.jp/s/ef83e38a72994a75bb4d097bfa5418dc https://tracery.jp/s/ef83e38a72994a75bb4d097bfa5418dc
