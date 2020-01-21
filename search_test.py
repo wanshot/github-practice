b555c02918fac0f87e0de0dc0bf9de89a151bd12
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-01-21 15:00:00 +0900

    .

--- search_test.py
+++ search_test.py
@@ -1,7 +1,1 @@
-http://bp.lvh.me:8000/projects/bp/pages/1
-http://bp.lvh.me:8000/projects/bp/pages/2
-
-http://lvh.me:8000/s/3e906d8db66c49e7a5bbe5a0bd555740
-
-
-http://bp.lvh.me:8000/projects/bp/pages/9
+delete
