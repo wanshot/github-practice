c88fcd1c1d6959c3643530e6cb78d52b6eef7fa3
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-02-13 16:30:17 +0900

    .

--- test6.py
+++ test6.py
@@ -1,6 +1,5 @@
-http://lvh.me:8000/s/138d0cfe5b414c14866bce720866c64e
+    """
 
-
-
-
-http://bp.lvh.me:8000/projects/bp/pages/9
+    http://lvh.me:8000/s/322ff4dd097e49978a80bc7a98d8e18d
+    """
+    pass
