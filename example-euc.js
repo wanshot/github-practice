e37bde1bdd778373c82dd14d9119616199b9087d
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-02-17 09:52:28 +0900

    ,

--- example-euc.js
+++ example-euc.js
@@ -3,6 +3,7 @@ class User {
   constructor (login) {
         this.login = login;
     }
+
     greet (){
         // example-euc.js
         console.log("みなさんこんにちは。私のハンドルネームは " + this.login + " です。よろしくお願いします。");
--- example-sjis.js
+++ example-sjis.js
@@ -3,6 +3,7 @@ class User {
     constructor (login) {
         this.login = login;
     }
+
     greet (){
         // example-sjis.js
         console.log("みなさんこんにちは。私のハンドルネームは " + this.login + " です。よろしくお願いします。");
