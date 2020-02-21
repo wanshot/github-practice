48f0b6066248b4dfd101b0f4b68b0649e6156c95
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-02-21 11:27:03 +0900

    .

---
+++ main.py
@@ -0,0 +1,92 @@
+def term():
+    """用語
+    URL
+    * http://bp.lvh.me:8000/projects/bp/terms/9
+    パーマリンク
+    * http://lvh.me:8000/s/2eb0c2992da44541967f5a859b16f4fd
+    """
+    pass
+
+
+def page():
+    """ページ
+    URL
+    * http://bp.lvh.me:8000/projects/bp/pages/9
+    パーマリンク
+    * http://lvh.me:8000/s/138d0cfe5b414c14866bce720866c64e
+    """
+    pass
+
+
+def page_comment():
+    """ページコメント
+    URL
+    * http://bp.lvh.me:8000/projects/bp/pages/9/#comment-8
+    パーマリンク
+    * http://lvh.me:8000/s/1855effe639f413993ef1c92ded30291
+    """
+    pass
+
+
+def diagram():
+    """図
+    URL
+    * http://bp.lvh.me:8000/projects/bp/diagrams/14
+    パーマリンク
+    * http://lvh.me:8000/s/ca5574e110154362889ee2140747bb1a
+    """
+    pass
+
+
+def diagram_item():
+    """図の説明
+    URL
+    * http://bp.lvh.me:8000/projects/bp/diagrams/14/edit/#item-6
+    パーマリンク
+    * http://lvh.me:8000/s/56db5ca084994e7cb09cc96a8759585c
+    """
+    pass
+
+
+def diagram_comment():
+    """図のコメント
+    URL
+    * http://bp.lvh.me:8000/projects/bp/diagrams/14/edit/#note-6
+    パーマリンク
+    * http://lvh.me:8000/s/3312510fec344abb92a21e5d71efd02f
+    """
+    pass
+
+
+def database():
+    """データベース
+    URL
+    * http://bp.lvh.me:8000/projects/bp/databases/1
+    パーマリンク
+    * http://lvh.me:8000/s/95f5c136936a4a079be3fe8c418d3098
+    """
+    pass
+
+
+def table():
+    """テーブル
+    URL
+    * http://bp.lvh.me:8000/projects/bp/databases/1/tables/21
+    パーマリンク
+    * http://lvh.me:8000/s/e961f66140344984a76bb3ecbd964ff3
+    """
+    pass
+
+
+def column():
+    """カラム
+    URL
+    * http://bp.lvh.me:8000/projects/bp/databases/1/tables/21#column-64
+    パーマリンク
+    * http://lvh.me:8000/s/e676d20346714b36855fdaa76298528e
+    """
+    pass
+
+
+# 連携していないプロジェクトのURL
+# http://lvh.me:8000/s/d9e2a3cd3d764360a4c11dc371f73bf9
