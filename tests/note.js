466218b4d2ee37910e77a6e11300bb79104e22c6
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-03-06 14:45:12 +0900

    .

--- tests/note.js
+++ tests/note.js
@@ -9,7 +9,7 @@ var gh = new GitHub({
     */
 });
 // ノートパーマリンクの差し替え -> 2回目のPUSHで説明にさしかえ
-// http://lvh.me:8000/s/3632aaa3c3c341c59ab20ca36e96acfc
+// http://lvh.me:8000/s/23d24823b4484db7b3aa9e2a731399f6
 
 var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
 me.listNotifications(function(err, notifications) {
