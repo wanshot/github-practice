dfc14fb6328d308a074ae10dc120ce6c00826ef5
wan <nishikawa0228@sj9.so-net.ne.jp>
2020-04-24 10:29:12 +0900

    .

---
+++ tes/2/test2.py
@@ -0,0 +1,78 @@
+class Page:
+    """http://lvh.me:8000/s/9bd90243d42a4ca4a5dd59f185bfdebe
+    """
+
+    def __init__(self):
+        """
+        """
+
+
+class PageComment:
+    """http://lvh.me:8000/s/c2ffed0ffdef413d89574e2465729f70
+    """
+
+    def __init__(self):
+        """
+        """
+
+
+class Diagram:
+    """http://lvh.me:8000/s/17984fdad3ff4783a41181788ef538b9
+    """
+
+    def __init__(self):
+        """
+        """
+
+
+class DiagramItem:
+    """http://lvh.me:8000/s/48c0d8bfb5fb4525917f51b6199668a0
+    """
+
+    def __init__(self):
+        """
+        """
+
+
+class Note:
+    """http://lvh.me:8000/s/018bf772cd954dc5a753db957ed0260b
+    """
+
+    def __init__(self):
+        """
+        """
+
+
+class Database:
+    """http://lvh.me:8000/s/01df7d35cc1e40628af0a9b1267f3232
+    """
+
+    def __init__(self):
+        """
+        """
+
+
+class Table:
+    """http://lvh.me:8000/s/ecb5248dea454926ad59883bd4c6cd79
+    """
+
+    def __init__(self):
+        """
+        """
+
+
+class Column:
+    """http://lvh.me:8000/s/6bd4ecda542c4156a6dbf37e65e9648c
+    """
+
+    def __init__(self):
+        """
+        """
+
+
+class Other:
+    """
+    """
+
+    def __init__(self):
+        pass
