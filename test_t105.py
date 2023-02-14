a4b61d035a745f9d639c135358e9f57035f00ba3
wan <nishikawa0228@sj9.so-net.ne.jp>
2023-02-14 13:43:09 +0900

    [#105] .

--- test_t105.py
+++ test_t105.py
@@ -5,3 +5,10 @@ def test():
     https://tracery.jp/s/5839427f106f4b2383bbb3e6257f1d64
     """
     return
+
+
+def test2():
+    """
+    https://tracery.jp/s/2c819847824c4e14a0838a94f040877e
+    """
+    return
